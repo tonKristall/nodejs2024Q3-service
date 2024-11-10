ee3773b62ca7874238d8f1bd50bcd211565fe8a2
Aleksander Shustov <krisale@yandex.ru>
2024-11-10 18:00:03 +0300

    feat: add swagger

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
+import { SwaggerModule, OpenAPIObject } from '@nestjs/swagger';
+import { readFileSync } from 'fs';
+import * as yaml from 'js-yaml';
 import 'dotenv/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const api = yaml.load(
+    readFileSync('./doc/api.yaml', 'utf8'),
+  ) as OpenAPIObject;
+  SwaggerModule.setup('doc', app, api);
+
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(process.env.PORT || 4000);
 }
